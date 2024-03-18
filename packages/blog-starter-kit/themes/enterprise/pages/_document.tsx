064c5601fb7139e799ea09412ea46c351c3b19ef
Dennis Muvaa <dmuvaa70@gmail.com>
2024-03-18 13:32:24 +0300

    Update Google Adsense Connection

--- packages/blog-starter-kit/themes/enterprise/pages/_document.tsx
+++ packages/blog-starter-kit/themes/enterprise/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
 		<Html lang="en">
 			<Head />
 			<meta name="google-site-verification" content="53DBsG2rwYW4NK6bwSs44fkzB1pHspUywS0KdGBw1yU" />
+			<meta name="google-adsense-account" content="ca-pub-4491990777755508"></meta>
 			<body>
 				<Main />
 				<NextScript />
